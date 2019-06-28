420133f58d69d3fa6244c045d57c2ba7b40fc5f4
elliot.melvin <elliot.melvin@cdl.co.uk>
2019-06-28 15:59:48 +0100

    Removes heading text when nav bar is opened

--- cart.js
+++ cart.js
@@ -2,4 +2,9 @@ $(document).ready(function(){
   $("#cart").on("click", function() {
     $(".shopping-cart").fadeToggle( "fast");
   });
+  $('input[type="checkbox"]').on('change', function() {
+    $(this).closest('div').find('.headingtext').toggle(!this.checked);
+
+    });
+  
 })();

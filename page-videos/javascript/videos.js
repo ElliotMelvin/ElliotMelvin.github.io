fe7561e6276a8d6720dac828510a1778a8226fc0
elliot.melvin <elliot.melvin@cdl.co.uk>
2019-08-28 14:55:31 +0100

    Added title to videos

--- page-videos/javascript/videos.js
+++ page-videos/javascript/videos.js
@@ -40,8 +40,30 @@ $(document).ready(function() {
           type: 'iframe',
           mainClass: 'mfp-fade',
           preloader: true,
+
       });
   });
 
+  $(document).ready(function() {
+  $('.popup-youtube').magnificPopup({
+    type: 'iframe',
+
+
+    iframe: {
+       markup: '<div class="mfp-iframe-scaler">'+
+                  '<div class="mfp-close"></div>'+
+                  '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+                  '<div class="mfp-title">Some caption</div>'+
+                '</div>'
+    },
+    callbacks: {
+      markupParse: function(template, values, item) {
+       values.title = item.el.attr('title');
+      }
+    }
+
+    });
+    
+  });
 
   })();
